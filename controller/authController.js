d7506286f97924048dcbb2f21fbeff815698544d
rishabh3562 <rishabh.dubey.cs21@ggits.net>
2025-11-21 14:45:40 +0530

    auth 2 : p1 : controller basic

--- controller/authController.js
+++ controller/authController.js
@@ -1,15 +1,36 @@
+const User = require("../model/userSchema");
+
 const loginHandler = (req, res) => {
 
 }
-const registerHandler = (req, res) => {
+const registerHandler = async (req, res) => {
+    // 1
+    const { name, age, email, password, role } = req.body;
 
+    //3
+    let data = new User({
+        name: name,
+        age: age,
+        email: email,
+        password: password,
+        role: role
+    })
+    let savedUser = await data.save();
+
+    //4
+    res.json({
+        success: true,
+        data: savedUser
+    });
+
+    //5
 }
 
 const logoutHandler = (req, res) => {
 
 }
 
-const  meHandler = (req, res) => {
+const meHandler = (req, res) => {
 
 }
-module.exports = { loginHandler ,registerHandler,meHandler,logoutHandler}
+module.exports = { loginHandler, registerHandler, meHandler, logoutHandler }
