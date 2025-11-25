2260696498d4697ff5304d79e4124cd80036925c
rishabh3562 <rishabh.dubey.cs21@ggits.net>
2025-11-25 09:51:49 +0530

    bcrypt

--- model/userSchema.js
+++ model/userSchema.js
@@ -1,13 +1,22 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 const userSchema = mongoose.Schema({
     name: String,
     age: Number,
-    email :String,
-    password:String,
-    role : {
-        type:String ,
-        enum:['admin','user']
+    email: String,
+    password: String,
+    role: {
+        type: String,
+        enum: ['admin', 'user']
     }
+})
+
+userSchema.pre('save', async function (next) {
+    let salt = await bcrypt.genSalt(10);
+    let hassedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hassedPassword;
+    next();
+
 })
 console.log("schema")
 const User = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose')
 const connectDB = require('./config/database')
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require('./routes/authRoutes');
-require('dotenv').config();
+require('dotenv').config({
+    quiet: true
+});
 const app = express();
 
 app.use(express.json())
